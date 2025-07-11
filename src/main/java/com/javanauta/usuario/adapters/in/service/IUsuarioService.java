644b2806a6895ba08f032c7982692ed97c4e6185
dev-LierBag <gabrielaf2186@gmail.com>
2025-07-10 23:10:04 -0300

    "Refatoração do metodo do login e autenticacao de usuarios"

--- src/main/java/com/javanauta/usuario/adapters/in/service/IUsuarioService.java
+++ src/main/java/com/javanauta/usuario/adapters/in/service/IUsuarioService.java
@@ -1,11 +1,15 @@
 package com.javanauta.usuario.adapters.in.service;
 
+import com.javanauta.usuario.adapters.in.dto.request.UsuarioDtoRequest;
 import com.javanauta.usuario.adapters.in.dto.request.UsuarioUpdateDtoRequest;
 import com.javanauta.usuario.adapters.in.dto.response.UsuarioDtoResponse;
+import com.javanauta.usuario.application.infrastructure.exceptions.UnauthorizedException;
 
 public interface IUsuarioService {
 
     UsuarioDtoResponse salvaUsuario(UsuarioDtoResponse dto);
+
+    String autenticarUsuario (UsuarioDtoRequest usuarioDtoRequest) throws UnauthorizedException;
 
     UsuarioDtoResponse buscarUsuarioPorEmail(String email);
 
--- src/main/java/com/javanauta/usuario/application/infrastructure/security/JwtRequestFilter.java
+++ src/main/java/com/javanauta/usuario/application/infrastructure/security/JwtRequestFilter.java
@@ -1,9 +1,14 @@
 package com.javanauta.usuario.application.infrastructure.security;
 
+import com.javanauta.usuario.application.infrastructure.exceptions.ResourceNotFoundException;
+import com.javanauta.usuario.application.infrastructure.exceptions.UnauthorizedException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,39 +31,47 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     }
 
     // Método chamado uma vez por requisição para processar o filtro
+    @SneakyThrows
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
 
-        // Obtém o valor do header "Authorization" da requisição
-        final String authorizationHeader = request.getHeader("Authorization");
+        try {
 
-        // Verifica se o cabeçalho existe e começa com "Bearer "
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            // Extrai o token JWT do cabeçalho
-            final String token = authorizationHeader.substring(7);
-            // Extrai o nome de usuário do token JWT
-            final String username = jwtUtil.extractUsername(token);
+            // Obtém o valor do header "Authorization" da requisição
+            final String authorizationHeader = request.getHeader("Authorization");
 
-            // Se o nome de usuário não for nulo e o usuário não estiver autenticado ainda
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                // Carrega os detalhes do usuário a partir do nome de usuário
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                // Valida o token JWT
-                if (jwtUtil.validateToken(token, username)) {
-                    System.out.println("Usuário autenticado com sucesso");
-                    // Cria um objeto de autenticação com as informações do usuário
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities());
-                    // Define a autenticação no contexto de segurança
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }else{
-                    System.out.println("Token inválido ou expirado");
+            // Verifica se o cabeçalho existe e começa com "Bearer "
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                // Extrai o token JWT do cabeçalho
+                final String token = authorizationHeader.substring(7);
+                // Extrai o nome de usuário do token JWT
+                final String username = jwtUtil.extractUsername(token);
+
+                // Se o nome de usuário não for nulo e o usuário não estiver autenticado ainda
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    // Carrega os detalhes do usuário a partir do nome de usuário
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    // Valida o token JWT
+                    if (jwtUtil.validateToken(token, username)) {
+                        System.out.println("Usuário autenticado com sucesso");
+                        // Cria um objeto de autenticação com as informações do usuário
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities());
+                        // Define a autenticação no contexto de segurança
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    } else {
+                        System.out.println("Token inválido ou expirado");
+                    }
                 }
             }
-        }
 
-        // Continua a cadeia de filtros, permitindo que a requisição prossiga
-        chain.doFilter(request, response);
+            // Continua a cadeia de filtros, permitindo que a requisição prossiga
+            chain.doFilter(request, response);
+
+        }catch (ExpiredJwtException | MalformedJwtException e){
+            throw new UnauthorizedException("Erro token invalido ou expirado ", e.getCause());
+        } catch (ServletException | IOException e) {
+            throw new ResourceNotFoundException("Erro ao realizar a autenticação: ", e.getCause());
+        }
     }
 }
